1ec167aa2aabbfc867bacac83cd6e08d451e6763
Fabius716 <33468311+Fabius716@users.noreply.github.com>
2020-01-30 17:51:56 +0100

    Add files via upload

--- Strange things i tried/floatStore.c
+++ Strange things i tried/floatStore.c
@@ -3,28 +3,24 @@
 
 int main()
 {
-    char stringCatcher[30];
+    int stringCatcher;
     float total;
 
     printf("Hello, welcome to the Wendios Shop. What would you like?\n");
     printf("Menu:\n     1.Poison burger:4.50€,\n     2.Coke:6.30€,\n     3.Cheeseless burger:2.40€\n");
-    scanf("%s",stringCatcher);
-    if (stringCatcher=="1")
+    scanf("%d",&stringCatcher);
+    if (stringCatcher==1)
     {
         total=total+4.5;
     };
-    if (stringCatcher=="2")
+    if (stringCatcher==2)
     {
         total=total+6.3;
     };
-    if (stringCatcher=="3")
+    if (stringCatcher==3)
     {
         total=total+2.4;
     };
-    bool ca = (stringCatcher=="2");
-
-    printf("%f,boolean :%zu",total,ca);
-    printf("%s",stringCatcher);*/
     printf("That will be %.2f€, thanks.\n",total);
     return 0;
 }
